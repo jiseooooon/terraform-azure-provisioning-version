64c17d590bef50abc340c9432b878fb3e6a213e4
jiseooooon <57252187+jiseooooon@users.noreply.github.com>
2022-04-21 18:07:15 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "username" {
+  description = "admin username"
+}
+
+variable "password" {
+  description = "admin password"
+}
+
+variable "instance_name" {
+  description = "vm instance name"
+}
